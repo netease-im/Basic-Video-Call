46b0d3215d444eca6c6ba46615a6dbb4d8f005bc
leeing <372711472@qq.com>
2020-09-22 20:28:43 +0800

    同步 g2 web demo 示例源码

---
+++ Group-Video/NERtcSample-GroupVideoCall-Web-Vue/vue.config.js
@@ -0,0 +1,23 @@
+/*
+* vue config
+* */
+const fs = require('fs-extra')
+module.exports = {
+    publicPath: '/',
+    devServer: {
+        port: 8020,
+        overlay: {
+            warnings: false,
+            errors: true
+        },
+        https: true,
+        key: fs.readFileSync('./cert/key.pem'),
+        cert: fs.readFileSync('./cert/cert.pem'),
+        openPage: '#/?path=multiple'
+    },
+    configureWebpack: config => {
+        if (process.env.NODE_ENV === 'development') {
+            config.devtool = 'eval-source-map'
+        }
+    }
+}
--- One-to-One-Video/NERtcSample-1to1-Web-Vue/src/main.js
+++ One-to-One-Video/NERtcSample-1to1-Web-Vue/src/main.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementUI from 'element-ui'
 import './assets/styles/common.less'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
+
+Vue.use(ElementUI)
 
 new Vue({
     router,
