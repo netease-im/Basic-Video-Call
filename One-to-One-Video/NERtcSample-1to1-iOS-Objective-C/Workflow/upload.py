178ad96763f74d8b0342a811b19008de4873cb88
WenchaoD <dingwenchao@corp.netease.com>
2020-09-26 16:53:58 +0800

    Update upload.py

--- One-to-One-Video/NERtcSample-1to1-iOS-Objective-C/Workflow/upload.py
+++ One-to-One-Video/NERtcSample-1to1-iOS-Objective-C/Workflow/upload.py
@@ -25,14 +25,18 @@ def upload(session,file):
 	file = open(FILE_PATH, 'rb')
 	print('file is {}'.format(file))
 	files = {'file': (os.path.basename(FILE_PATH), file, 'multipart/form-data', {'Expires': '0'})}
-	response = session.post(url, files=files).json()
-	print(json.dumps(response, indent=4, sort_keys=False))
+	response = session.post(url, files=files)
+	responseJson = response.json()
+	print(json.dumps(responseJson, indent=4, sort_keys=False))
 	file.close()
-	return response["data"]
+	response.close()
+	return responseJson["data"]
 	
 
 session = login('admin', 'admin123456')
 app_url = upload(session, FILE_PATH)
+
+session.close()
 
 print("Generate QRCode for url: {}".format(app_url))
 img = qrcode.make(app_url)
