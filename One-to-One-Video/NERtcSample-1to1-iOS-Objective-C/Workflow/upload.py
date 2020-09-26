f3e8ce0d90e92c14e4f2285087c60a5b8fa72167
WenchaoD <dingwenchao@corp.netease.com>
2020-09-26 13:40:24 +0800

    Update upload.py

--- One-to-One-Video/NERtcSample-1to1-iOS-Objective-C/Workflow/upload.py
+++ One-to-One-Video/NERtcSample-1to1-iOS-Objective-C/Workflow/upload.py
@@ -2,6 +2,8 @@ import requests
 import os
 import json
 import sys
+import fcntl
+import time 
 
 BASE_URL = sys.argv[1]
 FILE_PATH = sys.argv[2]
@@ -20,8 +22,20 @@ def login(username,password):
 def upload(session,file):
 	print("Uploading file {}".format(file))
 	url = BASE_URL + "/app/upload"
-	files = {'file': (os.path.basename(FILE_PATH), open(FILE_PATH, 'rb'), 'multipart/form-data', {'Expires': '0'})}
-	response = session.post(url, files=files).json()
+	response = None
+	while True:    
+		try:
+		    with open(FILE_PATH, 'rb') as io: 
+		        fcntl.flock(io, fcntl.LOCK_EX | fcntl.LOCK_NB)
+		        files = {'file': (os.path.basename(FILE_PATH), io, 'multipart/form-data', {'Expires': '0'})}
+		        response = session.post(url, files=files).json()
+		        fcntl.flock(io, fcntl.LOCK_UN)
+		        break 
+		except:
+		       #wait before retrying  
+				time.sleep(0.05)
+	# files = {'file': (os.path.basename(FILE_PATH), open(FILE_PATH, 'rb'), 'multipart/form-data', {'Expires': '0'})}
+	# response = session.post(url, files=files).json()
 	print(json.dumps(response, indent=4, sort_keys=False))
 	return response["data"]
 	
@@ -34,3 +48,5 @@ print("Downloading QRCode: {}".format(qrcode_url))
 io = open(QRCODE_OUTPUT, 'wb')
 io.write(requests.get(qrcode_url).content)
 io.close()
+
+
